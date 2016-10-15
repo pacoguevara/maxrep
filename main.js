ad04b991dd68c7958eabb72637c5634ffc8c985e
Paco Guevara <pacoguevaraa@gmail.com>
2016-10-15 01:03:23 -0500

    calc suggestions and fill the table

--- main.js
+++ main.js
@@ -10,10 +10,15 @@ var Calcs = (function () {
 
   var startCalcs = function(){
     var pr = $("#pr").val();
-    var percentage = (pr*(90/100));
-    var bottomSuggestion = percentage - percentage%5;
-    var topSuggestion = bottomSuggestion+5;
-    alert(bottomSuggestion+"~"+topSuggestion);
+		for (var i = 95; i >= 50; i-=5) {
+			var percentage = (pr*(i/100));
+	    var bottomSuggestion = percentage - percentage%5;
+	    var topSuggestion = bottomSuggestion+5;
+			$("#bottom"+i).html(bottomSuggestion);
+			$("#exactly"+i).html(percentage);
+	    $("#top"+i).html(topSuggestion);
+		}
+
   }
 
 	return {
